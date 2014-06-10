a7e93b63546ef0ac0aea7bb3036bdbf6f670db49
Plankton <Plankton>
2014-06-10 17:07:40 +0500

    Initial Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Plankton-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "PlanktonURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
